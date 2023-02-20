6aa1dff2ca7218c6e2f212dfe068cf70bc5b5ec2
EmileGrenon <egrenon@bell.net>
2023-02-20 15:11:09 -0500

    main

--- TP2/Assets/Script/CameraBorder.cs
+++ TP2/Assets/Script/CameraBorder.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class CameraBorder : MonoBehaviour
 {
-    Camera Camera;
+    [SerializeField] GameObject personnage;
+    Camera camera;
     float hauteur;
     float largeur;
+    float hauteurPersonnage;
+    float largeurPersonnage;
     // Start is called before the first frame update
     void Start()
     {
-        Camera = Camera.main;
+        camera = Camera.main;
+        hauteur = 2f * camera.orthographicSize;
+        largeur = hauteur * camera.aspect;
+        hauteurPersonnage = personnage.transform.localScale.y;
+        largeurPersonnage = personnage.transform.localScale.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float xMin = camera.transform.position.x - hauteur / 2 + largeurPersonnage / 2;
+        float xMax = camera.transform.position.x + hauteur / 2 - largeurPersonnage / 2;
+        float yMin = camera.transform.position.y - hauteur / 2 + hauteurPersonnage / 2;
+        float yMax = camera.transform.position.y + hauteur / 2 - hauteurPersonnage / 2;
+
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, xMin, xMax);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, yMin, yMax);
+        transform.position = clampedPosition;
     }
 }
