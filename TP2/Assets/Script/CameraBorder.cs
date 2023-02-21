497018fdd71944bdd86e18296d6ba707bd324598
EmileGrenon <egrenon@bell.net>
2023-02-20 21:19:58 -0500

    mouvementJoueur

--- TP2/Assets/Script/CameraBorder.cs
+++ TP2/Assets/Script/CameraBorder.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class CameraBorder : MonoBehaviour
 {
-    Camera Camera;
+    Camera camera;
     float hauteur;
     float largeur;
+    GameObject player;
+    float maxX;
+    float minX;
+    float maxY;
+    float minY;
     // Start is called before the first frame update
     void Start()
     {
-        Camera = Camera.main;
+        camera = Camera.main;
+        hauteur = camera.orthographicSize;
+        largeur = hauteur * camera.aspect;
+        player = GameObject.FindGameObjectWithTag("Player");
+        maxX = camera.transform.position.x + largeur - player.transform.localScale.x;
+        minX = camera.transform.position.x - largeur + player.transform.localScale.x;
+        maxY = camera.transform.position.y + hauteur - player.transform.localScale.y + player.transform.position.y;
+        minY = camera.transform.position.y - hauteur + player.transform.localScale.y + player.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        player.transform.position = new Vector3(Mathf.Clamp(player.transform.position.x, minX, maxX),
+            Mathf.Clamp(player.transform.position.y, minY, maxY), player.transform.position.z);
     }
 }
