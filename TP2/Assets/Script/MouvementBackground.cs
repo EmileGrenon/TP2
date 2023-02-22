48f3e637ab1548838ca3c18cbb71d2eb6545bc46
EmileGrenon <egrenon@bell.net>
2023-02-22 17:44:09 -0500

    bakcground

--- TP2/Assets/Script/MouvementBackground.cs
+++ TP2/Assets/Script/MouvementBackground.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class MouvementBackground : MonoBehaviour
 {
+    [SerializeField] float vitesse = 5;
     Camera cam;
-    MeshRenderer mr;
-    float camMax;
-    float camMin;
-    float hauteur;
+    SpriteRenderer sr;
+    float hauteurCam;
+    float hauteurMesh;
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-        mr = GetComponent<MeshRenderer>();
-        hauteur = mr.bounds.size.y / 2;
+        sr = GetComponent<SpriteRenderer>();
+        hauteurMesh = sr.bounds.size.y / 2;
+        hauteurCam = cam.orthographicSize * cam.aspect / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y + hauteurMesh < -hauteurCam + cam.transform.position.y)
+        {
+            transform.position = transform.position + new Vector3(0, hauteurMesh * 4 -1, 0);
+        }
+        transform.Translate(transform.up * -vitesse * Time.deltaTime);
     }
 }
