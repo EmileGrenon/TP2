2cc751af661b364cb8f2966fa54c4a69806ce5f4
Mikeleroni <mikaellevesque@me.com>
2023-02-22 16:39:27 -0500

    tire

--- TP2/Assets/Script/Joueur.cs
+++ TP2/Assets/Script/Joueur.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Joueur : MonoBehaviour
 {
     [SerializeField] float vitesse = 50;
     Rigidbody rb;
+    CharacterController cc;
+    [SerializeField] float vitesseTir = 3;
+    [SerializeField] GameObject balle;
+    GameObject barrel;
+
+    float delaiTir = 1;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        cc = GetComponent<CharacterController>();
+        barrel = GameObject.FindGameObjectWithTag("barrel");
     }
 
     // Update is called once per frame
     void Update()
     {
         Deplacement();
+
+        if (Input.GetAxis("Fire") > 0 && delaiTir <= 0)
+        {
+            //GameObject.Instantiate(balle,barrel.transform.position, barrel.transform.rotation);
+
+            GameObject obj = ObjectPool.objectPoolInstance.GetPooledObject(balle);
+
+            if (obj != null)
+            {
+                obj.transform.position = barrel.transform.position;
+                obj.transform.rotation = barrel.transform.rotation;
+                obj.SetActive(true);
+                obj = null;
+            }
+
+            delaiTir = 1 / vitesseTir;
+        }
+        else
+        {
+            delaiTir -= Time.deltaTime;
+        }
     }
 
     void Deplacement()
@@ -29,4 +59,6 @@ public class Joueur : MonoBehaviour
         }
         rb.MovePosition(transform.position + direction * vitesse * Time.deltaTime);
     }
+
 }
+
