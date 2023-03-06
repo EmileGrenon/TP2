d2fe570875eaf0160d25a9736a0b033565e56509
Mikeleroni <mikaellevesque@me.com>
2023-03-06 13:50:44 -0500

    changement

--- TP2/Assets/Script/estrateresttre.cs
+++ TP2/Assets/Script/estrateresttre.cs
@@ -6,23 +6,22 @@ public class estrateresttre : MonoBehaviour
 {
     [SerializeField] float amplitude = 3;
     [SerializeField] float radStepPerSecond = (float)(3.14159 / 2);
+    [SerializeField] GameObject tir;
     [SerializeField] Space space = Space.World;
+    GameObject barrel;
     float placeIni;
     // Start is called before the first frame update
     void Start()
     {
         placeIni = transform.position.x;
+        barrel = GameObject.FindGameObjectWithTag("barrel2");
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //transform.Translate(0, placeIni + (amplitude * Mathf.Sin(radStepPerSecond * Time.time))
-        //    - transform.position.y, 0, space);
         transform.Translate(placeIni + (amplitude * Mathf.Sin(radStepPerSecond * Time.time))
             - transform.position.x, (float)-0.005, 0, space);
-
     }
 
     private void OnCollisionEnter(Collision collision)
